624b5d875f1aa9d696da4afbd47f1cf9dc62e8c7
Labib Al Faisal <89539780+labib0x0hunter@users.noreply.github.com>
2025-05-03 11:30:14 +0600

    Update calculator.go

--- Ostad/Module02/calculator.go
+++ Ostad/Module02/calculator.go
@@ -8,85 +8,90 @@ import (
 	"strings"
 )
 
+func getParseError(err1, err2 error) error {
+	if err1 != nil && err2 != nil {
+		return fmt.Errorf("%v and %v", err1, err2)
+	} else if err1 != nil {
+		return fmt.Errorf("%v", err1)
+	} else if err2 != nil{
+		return fmt.Errorf("%v", err2)
+	}
+	return nil
+}
+
+func validateInput(input string) (a, b float64, operation string, err error) {
+	input = strings.TrimSpace(input)
+	splitInput := strings.Split(input, " ")
+	if len(splitInput) != 3 {
+		err = fmt.Errorf("invalid operation")
+		return
+	}
+	a, parseErrorA := strconv.ParseFloat(splitInput[0], 64)
+	b, parseErrorB := strconv.ParseFloat(splitInput[2], 64)
+	operation = splitInput[1]
+	err = getParseError(parseErrorA, parseErrorB)
+	return
+}
+
+func add(a, b float64) {
+	printResult(a, b, a+b, "+")
+}
+
+func subtract(a, b float64) {
+	printResult(a, b, a-b, "-")
+}
+
+func multiply(a, b float64) {
+	printResult(a, b, a*b, "*")
+}
+
+func division(a, b float64) {
+	if b == 0 {
+		printError(fmt.Errorf("can't divide by zero"))
+		return
+	}
+	printResult(a, b, a/b, "/")
+}
+
+func printError(err error) {
+	fmt.Println("Error:", err)
+}
+
+func printResult(a, b, result float64, operation string) {
+	fmt.Println(a, operation, b, "=", result)
+}
+
 func main() {
+
+	reader := bufio.NewReader(os.Stdin)
 
 	for {
 
-		// // fmt Input
-		// var (
-		// 	a, b           float64
-		// 	operation         string
-		// 	newLineOnBadInput string
-		// )
-		// fmt.Printf("Give two number : ")
-		// input, errInput := fmt.Scanf("%f %f", &a, &b)
-
-		// if errInput != nil || input != 2{
-		// 	fmt.Scanln(&newLineOnBadInput)
-		// 	fmt.Println("Error: ", errInput)
-		// 	// os.Exit(0)
-		// 	continue
-		// }
-
-		// fmt.Printf("Operation: ")
-		// _, errOperation := fmt.Scanf("%s", &operation)
-
-		// if errOperation != nil {
-		// 	fmt.Println("Error: ", errOperation)
-		// 	// os.Exit(0)
-		// 	continue
-		// }
-
-		// bufio Reader input
-		reader := bufio.NewReader(os.Stdin)
 		fmt.Printf("Give operation: ")
-		input, inputError := reader.ReadString('\n')
+		input, err := reader.ReadString('\n')
 
-		if inputError != nil {
-			fmt.Println("Error: ", inputError)
+		if err != nil {
+			printError(err)
 			continue
 		}
 
-		input = strings.Trim(input, "\n")
-		splitInput := strings.Split(input, " ")
-		if len(splitInput) != 3 {
-			fmt.Println("Error: Invalid Operation")
+		a, b, operation, err := validateInput(input)
+		if err != nil {
+			printError(err)
 			continue
 		}
-		a, parseErrorA := strconv.ParseFloat(splitInput[0], 64)
-		b, parseErrorB := strconv.ParseFloat(splitInput[2], 64)
-		operation := splitInput[1]
-
-		if parseErrorA != nil {
-			fmt.Println("Error: ", parseErrorA)
-			continue
-		}
-		if parseErrorB != nil {
-			fmt.Println("Error: ", parseErrorB)
-			continue
-		}
-
-		var result float64
 
 		switch operation {
 		case "+":
-			result = a + b
+			add(a, b)
 		case "-":
-			result = a - b
+			subtract(a, b)
 		case "*":
-			result = a * b
+			multiply(a, b)
 		case "/":
-			if b == 0 {
-				fmt.Println("Error: Can't Divide by zero")
-				// os.Exit(0)
-				continue
-			}
-			result = a / b
+			division(a, b)
 		default:
-			fmt.Println("Unknown Operation")
-			continue
+			printError(fmt.Errorf("unknown operation"))
 		}
-
-		fmt.Println(a, operation, b, "=", result)
 	}
 }
